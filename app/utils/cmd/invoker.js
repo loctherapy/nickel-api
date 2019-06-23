9a85510c322e35b0377454d4a411132710204b37
Egor Panok <egorpanok@gmail.com>
2019-06-23 23:21:12 +0300

    Log command only if the command result !== null

--- app/utils/cmd/invoker.js
+++ app/utils/cmd/invoker.js
@@ -2,7 +2,9 @@ let CommandService;
 
 async function run(cmd) {
     const res = await cmd();
-    CommandService.add(cmd.serialize());
+    if (res !== null) {
+        CommandService.add(cmd.serialize());
+    }
     console.log(`Command executed ${JSON.stringify(cmd.serialize())}`);
     return res;
 }
